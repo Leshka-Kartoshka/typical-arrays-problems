e3ae8140e073c6eef83a42d42a2e54ff0c812cf1
Lesha-Kartoshka <lesha.liho@gmail.com>
2021-02-06 18:28:37 +0300

    feat: typical array problems solve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array == undefined || array.length == 0){
+    return 0;
+  }
+  var min;
+  for(var i = 0; i < array.length; i++){
+    if(array[i] < min || min == undefined){
+      min = array[i];
+    }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array == undefined || array.length == 0){
+    return 0;
+  }
+  var max;
+  for(var i = 0; i < array.length; i++){
+    if(array[i] > max || max == undefined){
+      max = array[i];
+    }
+  }
+  return max;
 }
-
 exports.avg = function avg (array) {
-  return 0;
+  if(array == undefined || array.length == 0){
+    return 0;
+  }
+  var sum = 0;
+  for(var i = 0; i < array.length; i++){
+    sum += array[i];
+  }
+  var average = sum / array.length;
+  return average;
 }
